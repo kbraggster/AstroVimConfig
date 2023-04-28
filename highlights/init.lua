8b32bff24f8641b18783ec1b12f75676eb4ac026
Kyler Bragg <braggky@gmail.com>
2023-04-28 17:47:05 -0400

    Changed statusline color to only be custom color when using gruvbox theme. Else it uses default color for statusline

--- highlights/init.lua
+++ highlights/init.lua
@@ -1,5 +1,10 @@
+if vim.g.colors_name == "gruvbox" then
+  return {
+    StatusLine = { bg = "#1d2021" }
+  }
+end
+
 return {
   -- this table overrides highlights in all themes
   -- Normal = { bg = "#000000" },
-  StatusLine = { bg = "#1d2021" },
 }
